d74a17e941ae188ce95390719423ba5bf8cdd056
Rob Stokes <rob.lee.stokes@gmail.com>
2016-02-24 22:33:09 -0600

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: Caching compute intensive results using R scoping
+## makeCacheMatrix
+## cacheSolve
 
-## Write a short comment describing this function
+## Takes a matrix and returns a special "matrix"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) cachedInverse <<- inv
+  getInverse <- function() cachedInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Takes a special "matrix", calcuates and caches it's inverse.
+## Subsequent calls will return the cached result. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
 }
